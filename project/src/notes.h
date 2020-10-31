78f154ca15816afcdb7a59b3d521051ffdc2065e
FellowHashbrown <fellowhashbrown@gmail.com>
2020-10-31 08:16:50 -0600

    Added notes and stateMachine header file

---
+++ project/src/notes.h
@@ -0,0 +1,17 @@
+#ifndef notes_included
+#define notes_included
+
+#define C4 262
+#define C4sharp 277
+#define D4 294
+#define D4sharp 311
+#define E4 330
+#define F4 350
+#define F4sharp 370
+#define G4 392
+#define G4sharp 415
+#define A4 440
+#define A4sharp 466
+#define B4 494
+
+#endif
