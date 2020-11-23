3d9be161019515acd64d6f11b8902276b2cadaaf
FellowHashbrown <fellowhashbrown@gmail.com>
2020-11-22 23:21:29 -0700

    Updated stateMachine.c for song_advance function
    The song_advance function controls each song and how fast each song goes
    which is the same for each song through this implementation

--- project/src/stateMachine.c
+++ project/src/stateMachine.c
@@ -2,31 +2,56 @@
 #include <msp430.h>
 #include "stateMachine.h"
 #include "led.h"
+#include "buzz.h"
 #include "switches.h"
 
 void state_advance()
 {
   char changed = 0;
+  static char blink_count = 0;
   switch (switch_state_changed) {
   case 1:
-    toggle_green();
+    dim_75();
     changed = 1;
     break;
   case 2:
-    toggle_red();
+    dim_50();
     changed = 1;
     break;
   case 3:
-    red_on = 0;
-    green_on = 0;
-    toggle_red_green();
+    dim_25();
     changed = 1;
     break;
   case 4:
-    toggle_off();
+    if (++blink_count == 125) {
+      binary_led();
+      blink_count = 0;
+    }
     changed = 1;
     break;
   }
 
   led_changed = changed;
+}
+
+void song_advance()
+{
+  static char song_count = 0;
+  if (++song_count == 20) {
+    switch (switch_state_changed) {
+      case 1:
+	song_1();
+	break;
+      case 2:
+        song_2();
+	break;
+      case 3:
+	song_3();
+	break;
+      case 4:
+	stop();
+	break;
+    }
+    song_count = 0;
+  }
 }
--- project/src/stateMachine.h
+++ project/src/stateMachine.h
@@ -2,5 +2,6 @@
 #define stateMachine_included
 
 void state_advance();
+void song_advance();
 
 #endif
